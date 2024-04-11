0e5593b3e0f02d9e53ac7b50047b90ab991f9ad8
Aleksha <kotlitecture@gmail.com>
2024-04-11 10:15:19 +0300

    feat (todolist) Completed Tasks

--- app/src/main/kotlin/app/userflow/task/completed/CompletedTasksScreen.kt
+++ app/src/main/kotlin/app/userflow/task/completed/CompletedTasksScreen.kt
@@ -1,20 +1,19 @@
 package app.userflow.task.completed
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.paging.compose.collectAsLazyPagingItems
+import app.R
 import app.provideHiltViewModel
+import app.ui.container.FixedTopBarLazyColumnLayout
+import app.userflow.task.common.TaskList
 
 @Composable
 fun ActiveTasksScreen() {
     val viewModel: CompletedTasksViewModel = provideHiltViewModel()
-    Box(Modifier.fillMaxSize()) {
-        Text(
-            modifier = Modifier.align(Alignment.Center),
-            text = "Completed Tasks"
-        )
-    }
+    val items = viewModel.flowStore.asStateValue()?.collectAsLazyPagingItems()
+    FixedTopBarLazyColumnLayout(
+        title = stringResource(R.string.task_list_completed),
+        content = { TaskList(items, viewModel::onCheck) }
+    )
 }
--- app/src/main/kotlin/app/userflow/task/completed/CompletedTasksViewModel.kt
+++ app/src/main/kotlin/app/userflow/task/completed/CompletedTasksViewModel.kt
@@ -1,19 +1,40 @@
 package app.userflow.task.completed
 
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import app.AppState
+import app.data.TaskData
+import app.repository.TaskRepository
 import core.ui.BaseViewModel
-import core.ui.navigation.NavigationState
+import core.ui.state.StoreObject
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 @HiltViewModel
 class CompletedTasksViewModel @Inject constructor(
-    private val navigationState: NavigationState,
+    private val repository: TaskRepository,
     private val appState: AppState
 ) : BaseViewModel() {
 
-    fun onBack() {
-        navigationState.onBack()
+    val flowStore = StoreObject<Flow<PagingData<TaskData>>>()
+
+    override fun doBind() {
+        if (flowStore.isNull()) {
+            launchAsync("getAll") {
+                val all = repository.getAll(true)
+                flowStore.set(all.cachedIn(viewModelScope))
+            }
+        }
+    }
+
+    fun onCheck(data: TaskData?) {
+        if (data == null) return
+        launchAsync("onClick", appState) {
+            data.task.completed = !data.task.completed
+            repository.update(data)
+        }
     }
 
 }
