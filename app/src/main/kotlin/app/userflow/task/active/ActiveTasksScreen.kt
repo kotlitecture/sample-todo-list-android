a6435197ae99f2d6a760f503524a924d9a357406
Aleksha <kotlitecture@gmail.com>
2024-04-11 10:11:36 +0300

    feat (todolist) Active Tasks

--- app/src/main/kotlin/app/userflow/task/active/ActiveTasksScreen.kt
+++ app/src/main/kotlin/app/userflow/task/active/ActiveTasksScreen.kt
@@ -1,20 +1,77 @@
 package app.userflow.task.active
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ContentPasteGo
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.paging.compose.collectAsLazyPagingItems
+import app.R
 import app.provideHiltViewModel
+import app.ui.component.basic.ActionButton
+import app.ui.container.FixedHeaderFooterLazyColumnLayout
+import app.userflow.task.common.TaskList
+import core.ui.theme.ThemeData
 
 @Composable
 fun ActiveTasksScreen() {
     val viewModel: ActiveTasksViewModel = provideHiltViewModel()
-    Box(Modifier.fillMaxSize()) {
-        Text(
-            modifier = Modifier.align(Alignment.Center),
-            text = "Active Tasks"
-        )
-    }
+    val items = viewModel.flowStore.asStateValue()?.collectAsLazyPagingItems()
+    FixedHeaderFooterLazyColumnLayout(
+        header = {
+            Row(
+                modifier = Modifier.padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                CreateTaskTextBlock(Modifier.weight(1f), viewModel)
+                CreateTaskFromClipboardBlock(Modifier.wrapContentWidth(), viewModel)
+            }
+        },
+        content = {
+            TaskList(items, viewModel::onCheck)
+        }
+    )
+}
+
+@Composable
+private fun CreateTaskTextBlock(modifier: Modifier, viewModel: ActiveTasksViewModel) {
+    val noteStore = viewModel.noteStore
+    OutlinedTextField(
+        modifier = modifier.background(ThemeData.current.primary),
+        shape = RoundedCornerShape(12.dp),
+        value = noteStore.asStateValue().orEmpty(),
+        onValueChange = noteStore::set,
+        placeholder = { Text(text = stringResource(R.string.task_list_new)) },
+        trailingIcon = { CreteTaskActionBlock(Modifier, viewModel) }
+    )
+}
+
+@Composable
+private fun CreteTaskActionBlock(modifier: Modifier, viewModel: ActiveTasksViewModel) {
+    if (viewModel.noteStore.asStateValue().isNullOrBlank()) return
+    ActionButton(
+        modifier = modifier.padding(start = 16.dp),
+        onClick = { viewModel.onCreate() },
+        icon = Icons.Default.Add
+    )
+}
+
+@Composable
+private fun CreateTaskFromClipboardBlock(modifier: Modifier, viewModel: ActiveTasksViewModel) {
+    val clipboard = viewModel.clipboardStore.asStateValue() ?: return
+    ActionButton(
+        modifier = modifier.padding(start = 16.dp),
+        onClick = { viewModel.onCreate(clipboard) },
+        icon = Icons.Default.ContentPasteGo
+    )
 }
--- app/src/main/kotlin/app/userflow/task/active/ActiveTasksViewModel.kt
+++ app/src/main/kotlin/app/userflow/task/active/ActiveTasksViewModel.kt
@@ -1,19 +1,74 @@
 package app.userflow.task.active
 
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import app.AppState
+import app.data.TaskData
+import app.datasource.clipboard.AppClipboardSource
+import app.repository.TaskRepository
 import core.ui.BaseViewModel
-import core.ui.navigation.NavigationState
+import core.ui.state.StoreObject
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.distinctUntilChanged
 import javax.inject.Inject
 
 @HiltViewModel
 class ActiveTasksViewModel @Inject constructor(
-    private val navigationState: NavigationState,
+    private val clipboardSource: AppClipboardSource,
+    private val repository: TaskRepository,
     private val appState: AppState
 ) : BaseViewModel() {
 
-    fun onBack() {
-        navigationState.onBack()
+    val flowStore = StoreObject<Flow<PagingData<TaskData>>>()
+    val clipboardStore = StoreObject<String>()
+    val noteStore = StoreObject<String>()
+
+    override fun doBind() {
+        if (flowStore.isNull()) {
+            launchAsync("getAll") {
+                val all = repository.getAll(false)
+                flowStore.set(all.cachedIn(viewModelScope))
+            }
+        }
+        trackClipboard()
+    }
+
+    override fun doResume() {
+        trackClipboard()
+    }
+
+    fun onCheck(data: TaskData?) {
+        if (data == null) return
+        launchAsync("onClick", appState) {
+            data.task.completed = !data.task.completed
+            repository.update(data)
+        }
+    }
+
+    fun onCreate(text: String) {
+        launchAsync("onCreate", appState) {
+            repository.create(text)
+            clipboardSource.copy(null)
+        }
+    }
+
+    fun onCreate() {
+        val text = noteStore.get() ?: return
+        launchAsync("onCreate", appState) {
+            repository.create(text)
+            noteStore.clear()
+        }
+    }
+
+    private fun trackClipboard() {
+        launchAsync("trackClipboard") {
+            clipboardSource.getChanges()
+                .distinctUntilChanged()
+                .collectLatest(clipboardStore::set)
+        }
     }
 
 }
